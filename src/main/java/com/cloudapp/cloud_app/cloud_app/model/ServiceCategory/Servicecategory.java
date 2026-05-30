c30ba7cadd3b4c822561674878d5be093767d261
Shiddhant9879 <sidraynite@gmail.com>
2026-05-31 02:44:09 +0530

    added a ner model folder here on

---
+++ src/main/java/com/cloudapp/cloud_app/cloud_app/model/ServiceCategory/Servicecategory.java
@@ -0,0 +1,11 @@
+package com.cloudapp.cloud_app.cloud_app.model.ServiceCategory;
+
+public enum Servicecategory {
+
+    AC_REPAIR,
+    PLUMBING,
+    ELECTRICAL,
+    CARPENTRY,
+    APPLIANCE_REPAIR
+
+}
